9ad267899cde72b642d1e4ef1bd3cddcb5cb2510
chirstiankaikai <ckaikai19@gmail.com>
2022-09-28 16:38:21 -0400

    added routes

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const connection = require("./config/connection");
+const routes = require("./routes/user")
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/", routes);
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+});

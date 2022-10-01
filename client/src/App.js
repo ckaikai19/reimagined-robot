7ce6d1dd7db5133f7c068498557c17fd71bbf357
chirstiankaikai <ckaikai19@gmail.com>
2022-10-01 12:18:22 -0400

    fixed second page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Home from './pages/Home';
+import Details from "./pages/Details";
 import './styles/App.css';
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+
 
 function App() {
+  // const location = useLocation();
   return (
-    <div>
-      <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id"  element={<Details />} />
+      </Routes>
+    </Router>
   );
 }
 
